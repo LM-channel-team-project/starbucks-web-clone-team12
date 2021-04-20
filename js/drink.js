e915c544eb7fcde5645f0f67543d12a31a30ba36
lhk3337 <lhk3337@naver.com>
2021-04-20 20:05:59 +0900

    분류보기 추가

--- js/drink.js
+++ js/drink.js
@@ -1,18 +1,21 @@
-const productKindButton = document.querySelector(".product_kind_btn");
-const productKindButtonImage = document.querySelector("#kind_btn");
-const product_kind_check_view = document.querySelector("#product_kind_check");
+const drinkKindButton = document.querySelector(".drink_kind_btn");
+const drinkKindButtonImage = document.querySelector("#kind_btn");
+const drink_kind_check_view = document.querySelector("#drink_kind_check");
 let cnt = 1;
+
 const handleClick = () => {
   if (cnt % 2 == 1) {
-    productKindButtonImage.src =
+    drinkKindButtonImage.src =
       "https://image.istarbucks.co.kr/common/img/menu/list_down_btn.png";
-    product_kind_check_view.style.display = "none";
+    drink_kind_check_view.style.display = "none";
+    drinkKindButtonImage.setAttribute("alt", "분류보기 메뉴 펼치기");
   } else {
-    productKindButtonImage.src =
+    drinkKindButtonImage.src =
       "https://image.istarbucks.co.kr/common/img/menu/list_up_btn.png";
-    product_kind_check_view.style.display = "block";
+    drink_kind_check_view.style.display = "block";
+    drinkKindButtonImage.setAttribute("alt", "분류보기 메뉴 접기");
   }
   cnt++;
 };
 
-productKindButton.addEventListener("click", handleClick);
+drinkKindButton.addEventListener("click", handleClick);
