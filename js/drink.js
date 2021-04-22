ad6e8c6d9a5fccb3a3cd4aed390a8195de6bb75b
lhk3337 <lhk3337@naver.com>
2021-04-22 16:12:54 +0900

    footer부분 추가

--- js/drink.js
+++ js/drink.js
@@ -1,44 +1,52 @@
-const drinkKindButton = document.querySelector(".drink_kind_btn");
+"use strict";
 const drinkKindButtonImage = document.querySelector("#kind_btn");
 const drink_kind_check_view = document.querySelector("#drink_kind_check");
 const drinkKindTab = document.getElementById("drink_kind_tab");
 const drinkThemeTab = document.getElementById("theme_tab");
 const dt1Content = document.querySelector(".dt1_content");
 const dt2Content = document.querySelector(".dt2_content");
-let clicked = 0;
+const categoryList = document.querySelector(".categoryList");
+const themeList = document.querySelector(".themeList");
 
+let clicked = 0;
 const handleClick = () => {
   if (clicked) {
-    drinkKindButtonImage.src =
-      "https://image.istarbucks.co.kr/common/img/menu/list_down_btn.png";
-    drink_kind_check_view.style.display = "none";
-    drinkKindButtonImage.setAttribute("alt", "분류보기 메뉴 펼치기");
-    clicked = 0;
-  } else {
     drinkKindButtonImage.src =
       "https://image.istarbucks.co.kr/common/img/menu/list_up_btn.png";
     drink_kind_check_view.style.display = "block";
     drinkKindButtonImage.setAttribute("alt", "분류보기 메뉴 접기");
+    clicked = 0;
+  } else {
+    drinkKindButtonImage.src =
+      "https://image.istarbucks.co.kr/common/img/menu/list_down_btn.png";
+    drink_kind_check_view.style.display = "none";
+    drinkKindButtonImage.setAttribute("alt", "분류보기 메뉴 펼치기");
+
     clicked = 1;
   }
 };
 
-function drinkKindTabClick() {
+document
+  .querySelector(".drink_kind_btn")
+  .addEventListener("click", handleClick); //화살표 버튼 클릭시
+
+drinkKindTab.addEventListener("click", () => {
   drinkKindTab.classList.add("selected");
   drinkThemeTab.classList.remove("selected");
   dt1Content.style.display = "block";
   dt2Content.style.display = "none";
-}
-function drinkThemeTabClick() {
+  categoryList.style.display = "block";
+  themeList.style.display = "none";
+}); //카테고리 버튼 클릭시
+
+drinkThemeTab.addEventListener("click", () => {
   drinkKindTab.classList.remove("selected");
   drinkThemeTab.classList.add("selected");
   dt1Content.style.display = "none";
   dt2Content.style.display = "block";
-}
-
-drinkKindButton.addEventListener("click", handleClick); //화살표 버튼 클릭시
-drinkKindTab.addEventListener("click", drinkKindTabClick); //카테고리 버튼 클릭시
-drinkThemeTab.addEventListener("click", drinkThemeTabClick); //테마 버튼 클릭시
+  categoryList.style.display = "none";
+  themeList.style.display = "block";
+}); //테마 버튼 클릭시
 
 const loadItem = async () =>
   await fetch("../../data/drink.json")
