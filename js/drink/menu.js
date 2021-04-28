1ee23fe90a27b840a7c44d721dc0ff9ce9328c46
lhk3337 <lhk3337@naver.com>
2021-04-28 15:12:16 +0900

    js 파일 나누기

--- js/drink/menu.js
+++ js/drink/menu.js
@@ -47,25 +47,3 @@ drinkThemeTab.addEventListener("click", () => {
   categoryList.style.display = "none";
   themeList.style.display = "block";
 }); //테마 버튼 클릭시
-
-const loadItems = async () =>
-  await fetch("../../data/drink.json")
-    .then((response) => response.json())
-    .then((json) => json);
-
-const createHTMLString = (item) => {
-  return `
-        <li class="item">
-        <img src="${item.img}" alt="${item.category}" class="item_thumbnail" />
-        <span class="item_description">${item.title}</span>
-        </li>
-        `;
-};
-const displayItems = (items) => {
-  const container = document.querySelector(".list");
-  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
-};
-
-loadItems().then((items) => {
-  displayItems(items);
-});
