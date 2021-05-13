33d1d961ba6ad20c87fc5548e15ae33973f4a319
Kim-Hyerin <kor99lyn@naver.com>
2021-05-13 23:08:04 +0900

    [Rename] js/card.js into js/card/

---
+++ js/card/card.js
@@ -0,0 +1,18 @@
+/*const testBtn1 = document.querySelector("#test1");
+const testBtn2 = document.querySelector("#test2");
+
+testBtn1.addEventListener("click", 
+    )*/
+
+    function listClick1() {
+        document.querySelector("#category").style.backgroundColor = "#006633";
+        document.querySelector("#category").style.color = "#fff";
+        document.querySelector("#theme").style.backgroundColor = "#f4f4f1";
+        document.querySelector("#theme").style.color = "#000";
+    }
+    function listClick2() {
+        document.querySelector("#category").style.backgroundColor = "#f4f4f1";
+        document.querySelector("#category").style.color = "#000";
+        document.querySelector("#theme").style.backgroundColor = "#006633";
+        document.querySelector("#theme").style.color = "#fff";
+    }
