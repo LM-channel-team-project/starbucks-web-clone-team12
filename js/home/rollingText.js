3069925b40b08688b1f71cd180c9c3728afb570f
lhk3337 <lhk3337@naver.com>
2021-07-23 16:10:41 +0900

    make notice

---
+++ js/home/rollingText.js
@@ -0,0 +1,73 @@
+let rollingData = [
+  "홈페이지 이용약관 개정 안내",
+  "개인 정보처리 방침 개정 안내",
+  "시스템 개선 및 점검 안내",
+  "스타벅스 카드 이용약관 개정 안내",
+  "전자영수증 서비스 점검 안내",
+]; // 롤링할 데이터를 넣으면 됩니다 갯수 제한 없어요
+
+let timer = 2000; // 롤링되는 주기 입니다 (1000 => 1초)
+
+let first = document.getElementById("first"),
+  second = document.getElementById("second"),
+  third = document.getElementById("third");
+let move = 2;
+let dataCnt = 1;
+let listCnt = 1;
+
+//위 선언은 따로 완전히 수정하지 않는 한 조정할 필요는 없습니다.
+
+first.children[0].innerHTML = rollingData[0];
+
+setInterval(() => {
+  if (move == 2) {
+    first.classList.remove("card_sliding");
+    first.classList.add("card_sliding_after");
+
+    second.classList.remove("card_sliding_after");
+    second.classList.add("card_sliding");
+
+    third.classList.remove("card_sliding_after");
+    third.classList.remove("card_sliding");
+
+    move = 0;
+  } else if (move == 1) {
+    first.classList.remove("card_sliding_after");
+    first.classList.add("card_sliding");
+
+    second.classList.remove("card_sliding_after");
+    second.classList.remove("card_sliding");
+
+    third.classList.remove("card_sliding");
+    third.classList.add("card_sliding_after");
+
+    move = 2;
+  } else if (move == 0) {
+    first.classList.remove("card_sliding_after");
+    first.classList.remove("card_sliding");
+
+    second.classList.remove("card_sliding");
+    second.classList.add("card_sliding_after");
+
+    third.classList.remove("card_sliding_after");
+    third.classList.add("card_sliding");
+
+    move = 1;
+  }
+
+  if (dataCnt < rollingData.length - 1) {
+    document.getElementById("rolling__box").children[listCnt].children[0].innerHTML = rollingData[dataCnt];
+    dataCnt++;
+  } else if (dataCnt == rollingData.length - 1) {
+    document.getElementById("rolling__box").children[listCnt].children[0].innerHTML = rollingData[dataCnt];
+    dataCnt = 0;
+  }
+
+  if (listCnt < 2) {
+    listCnt++;
+  } else if (listCnt == 2) {
+    listCnt = 0;
+  }
+
+  console.log(listCnt);
+}, timer);
